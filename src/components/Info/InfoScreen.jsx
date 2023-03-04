70de26358ec690c5df4862f5230a7bfb82ab30a1
licornes-fluos <54073764+licornes-fluos@users.noreply.github.com>
2023-03-04 22:05:42 +0000

    simple button (basic)

---
+++ src/components/Info/InfoScreen.jsx
@@ -0,0 +1,10 @@
+import SimpleButton from "../UI/SimpleButton";
+
+function InfoScreen() {
+  return <div>
+    <SimpleButton NAME={"test"} PADDING={2} LOCATION={"/patient"} CLICK={true}></SimpleButton>
+
+  </div>
+}
+
+export default InfoScreen;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import SimpleButton from "./components/UI/SimpleButton";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
