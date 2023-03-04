5593e77f5897333ef92d999f480e55a05364be75
codrutiftode <codriniftode@gmail.com>
2023-03-04 21:17:05 +0000

    Add languages stub

---
+++ src/scripts/localStorage.js
@@ -0,0 +1,8 @@
+export const LocalStorage = {
+  get: (key) => {
+    return JSON.parse(localStorage.getItem(key));
+  },
+  set: (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+}
