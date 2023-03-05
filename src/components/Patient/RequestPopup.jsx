de6ed403609258f0efafc44e0377a0a5c7d5dac6
Owen Pauptit <owenpauptit@gmail.com>
2023-03-05 02:38:14 +0000

    Prettified the jank

--- src/components/Patient/RequestPopup.jsx
+++ src/components/Patient/RequestPopup.jsx
@@ -1,5 +1,6 @@
 import SimpleButton from "../UI/SimpleButton";
 import TextInput from "../UI/TextInput";
+import styled from "styled-components";
 
 function RequestPopup({closePopup}){
     const ClickHandler= () => {
@@ -8,12 +9,41 @@ function RequestPopup({closePopup}){
       }
 
     return(
-        <div>
-        <p>Would you like to add any extra information?</p>
-        <TextInput DEFAULT={"Add more detail here"} SIZE={"2em"} ></TextInput>
-        <SimpleButton padding={"1em"} name={"Send Request"} width={"10em"} height={"4em"} fontSize={"1em"} borderRadius={"1em"} ClickHandler={ClickHandler}></SimpleButton>
-        </div>
+        <Style.container>
+        <Style.question>Would you like to add any extra information?</Style.question>
+        <Style.input><TextInput DEFAULT={"Add more detail here"} SIZE={"2em"} ></TextInput></Style.input>
+        <Style.btn><SimpleButton name={"Send Request"} width={"10em"} height={"4em"} fontSize={"1em"} borderRadius={"1em"} ClickHandler={ClickHandler} background={(props) => props.theme.colors.primary}></SimpleButton></Style.btn>
+        </Style.container>
     )
 }
 
 export default RequestPopup
+
+const Style = {
+    container: styled.div`
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        padding: 5%;
+        padding-top: 30%;
+        padding-bottom: 30%;
+        height: 100%;
+    `,
+
+    question: styled.div`
+        display: inline-block;
+        text-align: center;
+        padding: 5%;
+    `,
+
+    input: styled.div`
+        display: inline-block;
+    `,
+
+    btn: styled.div`
+        display: inline-block;
+    `
+
+
+}
