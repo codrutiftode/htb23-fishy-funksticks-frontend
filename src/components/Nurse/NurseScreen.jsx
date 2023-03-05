8484a917d212b94ba06a951d847cb8f82f998584
codrutiftode <codriniftode@gmail.com>
2023-03-05 03:28:08 +0000

    Add scheduled break popup ui, fix popup styling

--- src/components/Nurse/NurseScreen.jsx
+++ src/components/Nurse/NurseScreen.jsx
@@ -4,29 +4,41 @@ import PopUpButton from "../UI/PopupButton";
 import styled from "styled-components";
 import NextTask from "./NextTask";
 import MainLayout from "../Layout/MainLayout";
+import Popup from "../UI/Popup";
+import ScheduledBreakPopup from "./ScheduledBreakPopup";
 
 function NurseScreen() {
   const [Break, SetBreak] = useState(false);
-  const [ShowPopup, SetShowPopup] = useState(false);
+  const [popupShown, setPopupShown] = useState(false);
+
+  const closePopup = () => {
+    setPopupShown(false);
+  };
+
+  const openPopup = () => {
+    setPopupShown(true);
+  };
 
   return (
     <MainLayout>
-    <Style.NurseScreen>
-      <h2>[Nurse Name]</h2>
-      <h3>Your next task</h3>
-      <NextTask />
-      <Style.OtherOptions>
-        <OnOffButton
-          NAME={"Pls Work"}
-          STATE={Break}
-          SetState={() => SetBreak((prev) => !prev)}
-        ></OnOffButton>
-        <PopUpButton name={"sKetchy"}></PopUpButton>
-      </Style.OtherOptions>
-    </Style.NurseScreen>
+      <Style.NurseScreen>
+        <h2>[Nurse Name]</h2>
+        <h3>Your next task</h3>
+        <NextTask />
+        <Style.OtherOptions>
+          <OnOffButton
+            NAME={"Pls Work"}
+            STATE={Break}
+            SetState={() => SetBreak((prev) => !prev)}
+          ></OnOffButton>
+          <PopUpButton name={"sKetchy"} onClick={openPopup}></PopUpButton>
+        </Style.OtherOptions>
+      </Style.NurseScreen>
+      <Popup shown={popupShown} closePopup={closePopup}>
+        <ScheduledBreakPopup closePopup={closePopup} />
+      </Popup>
     </MainLayout>
   );
-
 }
 
 export default NurseScreen;
