4c78b52ed217052047766c47380407e3dc1ec252
Anna Bloomfield <annaclare@Mist.local>
2023-03-05 02:04:28 +0000

    Language buttons now go to the login page after

--- src/components/Languages/LanguagesScreen.jsx
+++ src/components/Languages/LanguagesScreen.jsx
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 import LanguageItem from "./LanguageItem";
 import { LocalStorage } from "../../scripts/localStorage";
+import { useNavigate } from "react-router";
 
 function LanguagesScreen() {
   const languages = ["English", "Romanian", "French"];
+  const navigate = useNavigate();
 
   return (
     <Style.LanguagesList>
       {languages.map((language) => {
         const clickHandler = () => {
           LocalStorage.set("language", language);
+          navigate("/login");
         };
         return <LanguageItem language={language} onClick={clickHandler} />;
       })}
