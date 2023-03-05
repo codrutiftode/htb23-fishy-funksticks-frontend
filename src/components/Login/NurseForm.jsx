2e08b194433e26d08fb7f12182c26abeb0bb372f
Anna Bloomfield <annaclare@Mist.local>
2023-03-05 02:21:32 +0000

    improving flow functionality of buttons

--- src/components/Login/NurseForm.jsx
+++ src/components/Login/NurseForm.jsx
@@ -1,13 +1,22 @@
+import SimpleButton from "../UI/SimpleButton";
 import TextInput from "../UI/TextInput";
+import { useNavigate } from "react-router";
 
 
 function NurseForm(){
+    const navigate = useNavigate();
+    const ClickHandler = () => {
+        // sends the information
+        navigate("/nurse"); // needs to only execute if authenticated
+    }
+
     return(
         <div>        
         <p>Enter username:</p>
         <TextInput DEFAULT={"Blah"} SIZE={"0.5em"}></TextInput>
         <p>Enter password:</p>
         <TextInput DEFAULT={"Blah Blah"} SIZE={"0.5em"}></TextInput>
+        <SimpleButton name={"Submit"} ClickHandler={ClickHandler}></SimpleButton>
         </div>
 
     )
