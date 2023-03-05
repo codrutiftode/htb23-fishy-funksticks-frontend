9ba2ad3cfe9112031a6c338d0ec087880a1acbb3
licornes-fluos <54073764+licornes-fluos@users.noreply.github.com>
2023-03-05 01:22:26 +0000

    simple button MORE STYLE

--- src/components/Info/InfoScreen.jsx
+++ src/components/Info/InfoScreen.jsx
@@ -1,9 +1,13 @@
+import styled, { useTheme } from "styled-components";
 import SimpleButton from "../UI/SimpleButton";
 
 function InfoScreen() {
+    const theme = useTheme();
   return <div>
     <SimpleButton name={"test"} padding={"2em"} location={"/patient"} width={"50%"} 
-    height={"30%"} fontSize={"36px"} borderRadius={"10px"}></SimpleButton>
+    height={"30%"} fontSize={"36px"} borderRadius={"10px"} background={theme.colors.primary}
+    gradient={"linear-gradient(to right,rgb(117,71,163)0%, rgb(92,46,138)50%, rgb(71,36,107)100%)"}
+    textGradient={"white"}></SimpleButton> 
 
   </div>
 }
