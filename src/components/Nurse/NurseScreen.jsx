fb1d0c2b4d6843aae81b6f63d39a1b757691f430
codrutiftode <codriniftode@gmail.com>
2023-03-05 01:25:00 +0000

    Nurse screen basic structure

--- src/components/Nurse/NurseScreen.jsx
+++ src/components/Nurse/NurseScreen.jsx
@@ -1,19 +1,41 @@
 import OnOffButton from "../UI/OnOffButton";
 import { useState } from "react";
 import PopUpButton from "../UI/PopupButton";
+import styled from "styled-components";
+import NextTask from "./NextTask";
 
 function NurseScreen() {
-  const [Break, SetBreak] = useState(false)
-  const [ShowPopup, SetShowPopup] = useState(false)
+  const [Break, SetBreak] = useState(false);
+  const [ShowPopup, SetShowPopup] = useState(false);
 
-
-  return(
-    <div>
-    <OnOffButton NAME={"Pls Work"} STATE={Break} SetState={SetBreak}></OnOffButton>
-    <PopUpButton NAME ={"sKetchy"} State={ShowPopup} SetPOP={SetShowPopup}></PopUpButton>
-
-    </div>
-  )
+  return (
+    <Style.NurseScreen>
+      <h2>[Nurse Name]</h2>
+      <h3>Your next task</h3>
+      <NextTask />
+      <Style.OtherOptions>
+        <OnOffButton
+          NAME={"Pls Work"}
+          STATE={Break}
+          SetState={SetBreak}
+        ></OnOffButton>
+        <PopUpButton name={"sKetchy"}></PopUpButton>
+      </Style.OtherOptions>
+    </Style.NurseScreen>
+  );
 }
 
 export default NurseScreen;
+
+const Style = {
+  NurseScreen: styled.div`
+    display: flex;
+    padding: 1rem;
+    flex-direction: column;
+  `,
+
+  OtherOptions: styled.div`
+    display: flex;
+    flex-direction: row;
+  `,
+};
